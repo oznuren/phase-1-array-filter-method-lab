7079afe5616162a542e73beaa81fdc68e3b10edd
Öznur <oznur@tuta.io>
2022-04-21 11:56:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(name, str) {
+  return name.filter(function (el) {
+    return el.toLowerCase().indexOf(str.toLowerCase()) !== -1;
+  });
+}
+
+function fuzzyMatch(name, str) {
+  return name.filter(function (el) {
+    return el.startsWith(str);
+  });
+}
+
+function matchName(array, string) {
+  return array.filter((o) =>
+    Object.keys(o).some((k) =>
+      String(o[k]).toLowerCase().includes(string.toLowerCase())
+    )
+  );
+}
